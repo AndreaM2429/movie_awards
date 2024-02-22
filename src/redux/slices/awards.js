9436361d8eda0a2101df74c817924c60640c746e
Andrea Manuel <andreamanuel2207@gmail.com>
2024-02-22 11:09:04 -0600

    Use axios to fetch the movie data

--- src/redux/slices/awards.js
+++ src/redux/slices/awards.js
@@ -1,14 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://search.imdbot.workers.dev/?q=Niram';
+
+export const getMoviesInfo = createAsyncThunk('get/movies', async (thunkAPI) => {
+  try {
+    const resp = axios.get(url);
+    return resp;
+  } catch (error) {
+    return thunkAPI.rejectWithValue('Something went wrong');
+  }
+});
 
 const initialState = {
-  info: {},
-  state: 'loading',
+  movies: [],
+  status: 'loading',
   error: null,
 };
 
 const awardSlice = createSlice({
   name: 'awards',
   initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(getMoviesInfo.fulfilled, (state, action) => {
+        state.status = 'Succeeded';
+        const info = action.payload.data.description;
+        state.movies = info;
+      });
+  },
 });
 
 export default awardSlice.reducer;
