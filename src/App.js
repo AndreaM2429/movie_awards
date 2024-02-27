766fe62e3ca41b526830519ae57c7bc51eb5fb34
Andrea Manuel <andreamanuel2207@gmail.com>
2024-02-27 11:25:03 -0600

    Import the Movies component in App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import './App.css';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getMoviesInfo } from './redux/slices/awards';
+import Movies from './components/Movies';
 
 function App() {
-  const { movies } = useSelector((state) => state.movies);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getMoviesInfo());
-  }, [dispatch]);
-
-  console.log(movies);
-
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movies />
     </div>
   );
 }
