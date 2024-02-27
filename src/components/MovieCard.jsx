ef82fe729aa88aa9e089e743a637fcf1c3a0e9e1
Andrea Manuel <andreamanuel2207@gmail.com>
2024-02-27 11:22:46 -0600

    Create MovieCard component to display main information for each movie

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+function MovieCard({ MoviesArr }) {
+  return (
+    <div>
+      <h2>{MoviesArr.title}</h2>
+      <img src={MoviesArr.poster_img} alt={MoviesArr.title} />
+      <button type="button">Details</button>
+    </div>
+  );
+}
+
+MovieCard.propTypes = {
+  MoviesArr: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    poster_img: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
