e35db4dfedb879e88cbe62348e328cdc7894f9ef
Andrea Manuel <andreamanuel2207@gmail.com>
2024-02-28 10:27:29 -0600

    Change tags to section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import './styles/style.css';
 import Movies from './components/Movies';
 
 function App() {
   return (
-    <div className="App">
+    <section className="App">
       <Movies />
-    </div>
+    </section>
   );
 }
 
